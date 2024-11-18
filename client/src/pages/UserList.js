41fbfb2f20263a2b2614bc63690050f59ce3c7c5
KerolosAdib <keroadib@live.com>
2024-11-17 22:12:46 -0800

    Remove Apollo and use built in requests and move gql to server folder

--- client/src/pages/UserList.js
+++ client/src/pages/UserList.js
@@ -1,19 +1,17 @@
-import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import { useEffect } from "react";
 
-const GET_USERS = gql`
-    query ($page: Int!) {
+const GET_USERS = `
+    query ($page: Int!, $perPage: Int!) {
         tournaments(
             query: {
                 filter: { videogameIds: [43868], past: true }
-                perPage: 10
                 page: $page
+                perPage: $perPage
             }
         ) {
             nodes {
                 id
-                slug
-                events(filter: { videogameId: 43868 }) {
+                events(filter: { videogameId: 43868, published: true }) {
                     entrants {
                         nodes {
                             name
@@ -22,6 +20,7 @@ const GET_USERS = gql`
                                     discriminator
                                 }
                                 player {
+                                    id
                                     gamerTag
                                 }
                             }
@@ -33,27 +32,44 @@ const GET_USERS = gql`
     }
 `;
 
+async function FetchTournaments() {
+    var res = await fetch("http://localhost:3001/GetInfo", {
+        method: "GET",
+
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    console.log(res);
+    console.log(res.json());
+}
+
 export default function UserList() {
-    const { error, data, loading } = useQuery(GET_USERS, {
-        variables: { page: 1 },
+    useEffect(() => {
+        FetchTournaments();
     });
+    // const { error, data, loading } = useQuery(GET_USERS, {
+    //     variables: { page: 1, perPage: 5 },
+    // });
 
-    console.log({ error, loading, data });
-    if (loading) return <div>loading...</div>;
-    if (error) return <div>error</div>;
+    // console.log({ error, loading, data });
+    // if (loading) return <div>loading...</div>;
+    // if (error) return <div>error</div>;
 
-    console.log("Hello");
-    return data.tournaments.nodes[0].events[0].entrants.nodes.map((entrant) => {
-        const list = [];
-        entrant.participants.forEach((participant) => {
-            list.push(
-                <div key={participant.player.id}>
-                    {participant.player.gamerTag +
-                        ": " +
-                        participant.user.discriminator}
-                </div>
-            );
-        });
-        return <div>{list}</div>;
-    });
+    // const list = [];
+    // data.tournaments.nodes.forEach((tournament) => {
+    //     if (tournament.events.length > 0) {
+    //         tournament.events[0].entrants.nodes.forEach((entrant) => {
+    //             entrant.participants.forEach((participant) => {
+    //                 const tag = participant.player.gamerTag;
+    //                 list.push(
+    //                     <div key={tournament.id + participant.player.id}>
+    //                         {tag + ": " + participant.player.id}
+    //                     </div>
+    //                 );
+    //             });
+    //         });
+    //     }
+    // });
+    // return <div>{list}</div>;
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,47 @@
 const sql = require("mssql");
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+app.use(cors());
+
+const GET_USERS = `
+    query ($page: Int!, $perPage: Int!) {
+        tournaments(
+            query: {
+                filter: { videogameIds: [43868], past: true }
+                page: $page
+                perPage: $perPage
+            }
+        ) {
+            nodes {
+                id
+                events(filter: { videogameId: 43868, published: true }) {
+                    entrants {
+                        nodes {
+                            name
+                            participants {
+                                user {
+                                    discriminator
+                                }
+                                player {
+                                    id
+                                    gamerTag
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
 
 const config = {
     user: "adibk",
     password: "test",
-    server: "DESKTOP-4RJ0SV4\\SQLEXPRESS",
+    server: process.env.SQL_SERVER_NAME + "\\SQLEXPRESS",
     options: {
         encrypt: true,
         database: "StartggDB",
@@ -34,8 +69,47 @@ app.get("/", async (req, res) => {
     });
 });
 
+app.get("/GetInfo", async (req, res) => {
+    var data = await FetchTournaments();
+    console.log(JSON.stringify(data));
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(data));
+});
+
+async function FetchTournaments() {
+    var page = 1;
+    var hasMore = true;
+    var data = [];
+    while (hasMore && page <= 10) {
+        var results = await fetch("https://api.start.gg/gql/alpha", {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + process.env.AUTH_TOKEN,
+            },
+
+            body: JSON.stringify({
+                query: GET_USERS,
+                variables: {
+                    page: page,
+                    perPage: 5,
+                },
+            }),
+        });
+        results = await results.json();
+        hasMore =
+            results.data &&
+            results.data.tournaments &&
+            results.data.tournaments.nodes.length;
+        data.push(results);
+        page++;
+    }
+    return data;
+}
+
 app.post("/addUser", (req, res) => {});
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log("Listening...");
 });
