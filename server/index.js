32bb11ab67b2bed3a3c663c4a0ffe4aaa7d1a2c8
KerolosAdib <keroadib@live.com>
2025-01-01 15:24:33 -0800

    Add working player retrieval

--- server/index.js
+++ server/index.js
@@ -2,11 +2,158 @@ const sql = require("mssql");
 const express = require("express");
 const cors = require("cors");
 const PG = require("pg");
+const fs = require("fs");
+const path = require("path");
 
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
+
+const GET_TOURNAMENTS = `
+    query ($page: Int! $perPage: Int! $SF6: ID! $AfterDate: Timestamp!) {
+        tournaments(
+            query: {
+                filter: { 
+                    videogameIds: [
+                        $SF6
+                    ]
+                    past: true
+                    afterDate: $AfterDate
+                }
+                page: $page
+                perPage: $perPage
+                sortBy: "startAt asc"
+            }
+        ) {
+            nodes {
+                id
+                name
+                slug
+                startAt
+                updatedAt
+            }
+        }
+    }
+`;
+
+const GET_EVENTS = `
+    query ($perPage: Int! $tournamentIDs: [ID]! $SF6: ID!) {
+        tournaments(
+            query: {
+                filter: {
+                    ids: $tournamentIDs
+                }  
+                perPage: $perPage
+            }
+        ) {
+            nodes {
+                id
+                events(filter: {
+                    videogameId: [$SF6]
+                    published: true
+                    type: 1
+                }) {
+                    id
+                    name
+                    slug
+                    updatedAt
+                }
+            }
+        }
+    }
+`;
+
+const GET_PLAYERS_WITHIN_EVENT = `
+    query ($eventID: ID! $perPage: Int! $page: Int!) {
+        event(id: $eventID) {
+            slug
+            entrants(query: {
+                page: $page
+                perPage: $perPage
+            }) {
+                nodes {
+                    id
+                    name
+                    participants {
+                        player {
+                            id
+                            gamerTag
+                            user {
+                                slug
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+const GET_PLAYERS_WITHIN_EVENT_TEST = `
+    query ($tournamentIDs: [ID]! $eventIDs: [ID]! $eventsPerPage: Int! $eventsPage: Int! $entrantsPerPage: Int! $entrantsPage: Int!) {
+        tournaments(query: {
+            filter: {
+                ids: $tournamentIDs
+            }
+        }) {
+            nodes: {
+                events(filter: {
+                    ids: $eventIDs
+                    perPage: $eventsPerPage
+                    page: $eventsPage
+                }) {
+                    entrants(query: {
+                        perPage: $entrantsPerPage
+                        page: $entrantsPage
+                    }) {
+                        nodes: {
+                            participants: {
+                                player: {
+                                    id
+                                    gamerTag
+                                    user {
+                                        slug
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+const GET_SETS = `
+    query ($EventID: ID!) {
+        event(id: $EventID) {
+            sets {
+                nodes { 
+                    slots {
+                        standing {
+                            entrant {
+                                id
+                                participants {
+                                    player {
+                                        id
+                                        gamerTag
+                                    }
+                                }
+                            }
+                        }
+                        stats {
+                            score {
+                                value
+                            }
+                        }
+                    }
+                    winnerId
+                }
+            }
+        }
+    }
+`;
 
 const GET_USERS = `
     query ($page: Int!, $perPage: Int!, $SF6: ID!) {
@@ -16,7 +163,7 @@ const GET_USERS = `
                     videogameIds: [
                         $SF6
                     ]
-                    past: true 
+                    past: true
                 }
                 page: $page
                 perPage: $perPage
@@ -39,7 +186,7 @@ const GET_USERS = `
 `;
 
 const GET_SETS_FOR_ENTRANT = `
-    query($EntrantID: ID!) {
+    query($entrantID: ID!) {
         entrant(id: $EntrantID) {
             id
             name
@@ -80,18 +227,6 @@ const pg = new PG.Pool({
     port: process.env.PGPORT,
 });
 
-const config = {
-    user: "adibk",
-    password: "test",
-    server: process.env.SQL_SERVER_NAME + "\\SQLEXPRESS",
-    options: {
-        encrypt: true,
-        database: "StartggDB",
-        trustServerCertificate: true,
-        trustedConnection: true,
-    },
-};
-
 const delay = (time) => new Promise((resolve) => setTimeout(resolve, time));
 
 async function checkConnection() {
@@ -106,12 +241,9 @@ async function checkConnection() {
 
 async function CreateTables() {
     try {
-        var table =
-            "CREATE TABLE IF NOT EXISTS Players(" +
-            "PlayerID INT PRIMARY KEY CHECK (PlayerID > 0)," +
-            "GamerTag VARCHAR(255) NOT NULL," +
-            "TotalWins INT CHECK (TotalWins >= 0)," +
-            "TotalLosses INT CHECK (TotalLosses >= 0));";
+        var table = fs
+            .readFileSync(path.resolve(__dirname, "./tableCreation.sql"))
+            .toString();
         const res = await pg.query(table);
         console.log(res.rows);
     } catch (err) {
@@ -142,16 +274,135 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/GetInfo", async (req, res) => {
-    var data = await FetchTournaments();
+    var tournamentIds = await FetchTournaments();
+    var events = await RetrieveEventsBasedOnTournaments(tournamentIds);
+    var playerMap = await RetrievePlayersFromEvents(events, tournamentIds);
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(data));
+    res.end(JSON.stringify(tournamentIds));
 });
 
 async function FetchTournaments() {
-    var page = 1;
     var hasMore = true;
-    var data = [];
-    while (hasMore && page <= 10) {
+    var tournamentIds = [];
+    var date = new Date(1000);
+    var i = 1;
+    const getCurrentTournamentInfo = "SELECT * FROM Tournaments;";
+    const res = await pg.query(getCurrentTournamentInfo);
+    const map = res.rows.reduce((map, obj) => {
+        const { tournamentid, latestupdate, tournamentname, slug } = obj;
+        map[tournamentid] = { latestupdate, tournamentname, slug };
+        return map;
+    }, {});
+
+    while (hasMore) {
+        var page = 1;
+        while (page <= 20 && hasMore) {
+            var results = await fetch("https://api.start.gg/gql/alpha", {
+                method: "POST",
+
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + process.env.AUTH_TOKEN,
+                },
+
+                body: JSON.stringify({
+                    query: GET_TOURNAMENTS,
+                    variables: {
+                        page: page,
+                        perPage: 500,
+                        SF6: 43868,
+                        AfterDate: date.getTime() / 1000,
+                    },
+                }),
+            });
+            results = await results.json();
+            console.log(results);
+            hasMore = results.data.tournaments.nodes.length == 500;
+            var j = 1;
+            results.data.tournaments.nodes.forEach((tournament) => {
+                var totalIds = (i - 1) * 10000 + (page - 1) * 500 + j;
+                //var idIndex = Math.floor((totalIds - 1) / 10);
+                console.log(totalIds);
+                if (
+                    !map[tournament.id] ||
+                    (map[tournament.id] &&
+                        map[tournament.id].latestupdate / 1000 <
+                            tournament.updatedAt)
+                ) {
+                    var log = `
+                    {
+                        CurrentEntry: ${totalIds}
+                        id: ${tournament.id},
+                        Name: ${tournament.name},
+                        Slug: ${tournament.slug},
+                        startAt: ${tournament.startAt},
+                    }
+                    `;
+
+                    // fs.appendFile("log.txt", log, (err) => {
+                    //     if (err) throw err;
+                    // });
+
+                    const insert = `
+                        INSERT INTO Tournaments(TournamentID, LatestUpdate, TournamentName, Slug)
+                        VALUES($1, TO_TIMESTAMP($2), $3, $4)
+                        ON CONFLICT(TournamentID)
+                        DO UPDATE
+                        SET LatestUpdate = EXCLUDED.LatestUpdate,
+                            TournamentName = EXCLUDED.TournamentName,
+                            Slug = EXCLUDED.Slug
+                        WHERE Tournaments.LatestUpdate != EXCLUDED.LatestUpdate;
+                    `;
+                    pg.query(insert, [
+                        tournament.id,
+                        tournament.updatedAt,
+                        tournament.name,
+                        tournament.slug,
+                    ]);
+                    tournamentIds.push(tournament.id);
+                }
+                j++;
+            });
+
+            if (page == 20 && hasMore) {
+                date.setTime(
+                    results.data.tournaments.nodes[499].startAt * 1000
+                );
+            }
+            page++;
+            await delay(1000);
+        }
+        i++;
+    }
+    return tournamentIds;
+}
+
+async function RetrieveEventsBasedOnTournaments(tournamentIds) {
+    var outdatedEvents = [];
+    var totalEvents = 0;
+    var i = 0;
+    var groupSize = 500;
+    const groupedIds = [];
+    for (let i = 0; i < tournamentIds.length; i += groupSize) {
+        groupedIds.push(tournamentIds.slice(i, i + groupSize));
+    }
+
+    while (i < groupedIds.length) {
+        const getEventsQuery = "SELECT * FROM Events WHERE TournamentID = $1";
+        const existingEvents = [];
+        var j = 0;
+        while (j < groupedIds[i].length) {
+            var res = await pg.query(getEventsQuery, [groupedIds[i][j]]);
+            if (res.rows) existingEvents.push(res.rows);
+            j++;
+        }
+
+        const eventsMap = existingEvents.reduce((map, obj) => {
+            const { eventid, latestupdate, eventname, slug } = obj;
+            map[eventid] = { latestupdate, eventname, slug };
+            return map;
+        }, {});
+
         var results = await fetch("https://api.start.gg/gql/alpha", {
             method: "POST",
 
@@ -161,72 +412,191 @@ async function FetchTournaments() {
             },
 
             body: JSON.stringify({
-                query: GET_USERS,
+                query: GET_EVENTS,
                 variables: {
-                    page: page,
-                    perPage: 5,
+                    perPage: 500,
+                    tournamentIDs: groupedIds[i],
                     SF6: 43868,
                 },
             }),
         });
+
         results = await results.json();
-        hasMore =
-            results.data &&
-            results.data.tournaments &&
-            results.data.tournaments.nodes.length;
-        if (hasMore) {
-            results.data.tournaments.nodes.forEach((tournament) => {
-                tournament.events.forEach((event) => {
-                    event.entrants.nodes.forEach((entrant) => {
-                        data.push(entrant.id);
-                    });
-                });
+        console.log(results);
+        results.data.tournaments.nodes.forEach((tournament) => {
+            var id = tournament.id;
+            tournament.events.forEach((event) => {
+                if (
+                    !eventsMap[event.id] ||
+                    (eventsMap[event.id] &&
+                        eventsMap[event.id].latestupdate / 1000 <
+                            event.updatedAt)
+                ) {
+                    console.log(`EventID: ${event.id}`);
+                    console.log(`updatedAt: ${event.updatedAt}`);
+                    console.log(`EventName: ${event.name}`);
+                    console.log(`Slug: ${event.slug}`);
+                    console.log(`Total Events: ${totalEvents}`);
+                    totalEvents++;
+                    const insertOrUpdateEvent = `
+                        INSERT INTO Events(TournamentID, EventID, LatestUpdate, EventName, Slug)
+                        VALUES($1, $2, TO_TIMESTAMP($3), $4, $5)
+                        ON CONFLICT(EventID)
+                        DO UPDATE
+                        SET LatestUpdate = EXCLUDED.LatestUpdate,
+                            EventName = EXCLUDED.EventName,
+                            Slug = EXCLUDED.Slug
+                        WHERE Events.LatestUpdate != EXCLUDED.LatestUpdate
+                    `;
+
+                    pg.query(insertOrUpdateEvent, [
+                        id,
+                        event.id,
+                        event.updatedAt,
+                        event.name,
+                        event.slug,
+                    ]);
+                    outdatedEvents.push(event.id);
+                }
             });
-        }
-        page++;
+        });
+        i++;
+        await delay(1000);
     }
+    return outdatedEvents;
+}
 
+async function RetrievePlayersFromEvents(eventIds) {
+    const players = new Map();
     var i = 0;
-    while (i < data.length) {
-        var results = await fetch("https://api.start.gg/gql/alpha", {
-            method: "POST",
+    while (i < eventIds.length) {
+        var hasMore = true;
+        var j = 1;
+        while (hasMore) {
+            var results = await fetch("https://api.start.gg/gql/alpha", {
+                method: "POST",
 
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + process.env.AUTH_TOKEN,
-            },
-
-            body: JSON.stringify({
-                query: GET_SETS_FOR_ENTRANT,
-                variables: {
-                    EntrantID: data[i],
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + process.env.AUTH_TOKEN,
                 },
-            }),
-        });
 
-        results = await results.json();
-        console.log(i);
-        console.log(data.length);
-        console.log(results.data.entrant.id);
-        console.log(results.data.entrant.name);
-        results.data.entrant.paginatedSets.nodes.forEach((set) => {
-            console.log(set);
-            console.log(set.slots[0].entrant.participants);
-            console.log(set.slots[1].entrant.participants);
-        });
-        await delay(750);
+                body: JSON.stringify({
+                    query: GET_PLAYERS_WITHIN_EVENT,
+                    variables: {
+                        eventID: eventIds[i],
+                        perPage: 300,
+                        page: j,
+                    },
+                }),
+            });
+
+            console.log(eventIds[i]);
+            results = await results.json();
+            console.log(results);
+
+            hasMore = results.data.event.entrants.nodes.length != 0;
+
+            results.data.event.entrants.nodes.forEach((entrant) => {
+                if (entrant.participants[0]) {
+                    var playerId = entrant.participants[0].player.id;
+                    var playerGamerTag =
+                        entrant.participants[0].player.gamerTag;
+                    var playerSlug = "";
+                    if (entrant.participants[0].player.user) {
+                        playerSlug = entrant.participants[0].player.user.slug;
+                    }
+
+                    const insertOrUpdatePlayer = `
+                        INSERT INTO Players(PlayerID, GamerTag, TotalWins, TotalLosses, Slug)
+                        VALUES($1, $2, $3, $4, $5)
+                        ON CONFLICT(PlayerID)
+                        DO UPDATE
+                        SET GamerTag = EXCLUDED.GamerTag,
+                            Slug = EXCLUDED.Slug
+                    `;
+
+                    pg.query(insertOrUpdatePlayer, [
+                        playerId,
+                        playerGamerTag,
+                        0,
+                        0,
+                        playerSlug,
+                    ]);
+
+                    console.log(
+                        `${playerId}: ${entrant.participants[0].player.gamerTag}`
+                    );
+                    if (players.get(eventIds[i])) {
+                        players.get(eventIds[i]).push(playerId);
+                    } else {
+                        players.set(eventIds[i], [playerId]);
+                    }
+                }
+            });
+            j++;
+            await delay(750);
+        }
+        console.log(`${i}/${eventIds.length}`);
         i++;
     }
-    return data;
+    return players;
+}
+
+async function test() {
+    var date = new Date(1);
+    console.log(date);
+    date.setTime(1734307200 * 1000);
+    console.log(date);
+
+    try {
+        var query = "SELECT TournamentID, LatestUpdate FROM Tournaments;";
+        const res = await pg.query(query);
+        console.log(res.rows);
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 app.post("/addUser", (req, res) => {});
+
+app.get("/getPlayers", async (req, res) => {
+    const query = "SELECT EventId FROM Events;";
+    const results = await pg.query(query);
+    const eventIds = results.rows.map((obj) => {
+        return obj.eventid;
+    });
+    var playerMap = await RetrievePlayersFromEvents(eventIds);
+    console.log("Debug");
+});
+
+app.get("/test", async (req, res) => {
+    const query = "SELECT EventId FROM Events;";
+    const results = await pg.query(query);
+    const eventIds = results.rows.map((obj) => {
+        return obj.eventid;
+    });
+    console.log(eventIds);
+    console.log(results);
+});
+
+app.get("/clear", async (req, res) => {
+    const query = fs
+        .readFileSync(path.resolve(__dirname, "./tableClear.sql"))
+        .toString();
+
+    await pg.query(query);
+    console.log("Tables Cleared");
+    res.send("Tables Cleared");
+});
 
 checkConnection();
 
 CreateTables();
 
 ViewPlayers();
+
+test();
 
 app.listen(3001, () => {
     console.log("Listening...");
